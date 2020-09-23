e75f26d61af4f05474294ca003f11dfdf8939332
yoting <jbhbjjjj@gmail.com>
2020-09-23 10:13:14 +0800

    move

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Click from './Click';
+import Click from './src/Click';
 
 export default function App() {
   return (
